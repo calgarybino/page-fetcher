782f45b9417b96e67d777688cc4e8b359c17650c
calgarybino <binoandemeskel1234@gmail.com>
2023-06-04 21:16:41 -0400

    Add request library and update fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -1,11 +1,27 @@
+const fs = require("fs");
+const request = require("request");
+
 // Get command line arguments
-const url = process.argv[2];
-const filePath = process.argv[3];
+const [, , url, filePath] = process.argv;
 
+// Send the HTTP request and save the file
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error("Error:", error.message);
+    return;
+  }
 
+  if (response.statusCode !== 200) {
+    console.error("Error:", `Status code ${response.statusCode} received.`);
+    return;
+  }
 
-
-
-
-
-
+  // Write the received data to the file
+  fs.writeFile(filePath, body, (error) => {
+    if (error) {
+      console.error("Error:", error.message);
+      return;
+    }
+    console.log(`Downloaded and saved ${body.length} bytes to ${filePath}.`);
+  });
+});
